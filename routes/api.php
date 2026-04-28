38c24c583061cfc051f44f143ee16d9adac1aa20
Alrae Joseph Ordonez <ordonezalraejoseph85@gmail.com>
2026-04-28 21:29:32 +0800

    Putted the login inside V1

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::get('/test', function () {
 });
 
 // The login route must be public so users can actually log in!
-Route::post('/login', [LoginController::class, 'login']);
+
 
 
 // We use 'auth:sanctum' to ensure only logged-in users with a token can enter
@@ -40,7 +40,7 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
 
     Route::get('dashboard/stats', [StudentController::class, 'stats']);
     Route::get('notifications', [NotificationController::class, 'index']);
-
+    Route::post('/login', [LoginController::class, 'login']);
     Route::apiResource('students', StudentController::class);
     Route::get('students/{id}/violations', [StudentController::class, 'violations']);
     Route::get('students/{id}/subjects',   [StudentController::class, 'subjects']);
